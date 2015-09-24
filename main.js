97ad84772d4be7de2c87dba3f656f35276980e68
Student <student@Mac-mini.local>
2015-09-24 17:08:31 -0600

    final commit

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-angular.module('myApp',[])
+angular.module('myApp', ["ngRating"])
 angular.module('myApp').controller('newQuote', function ($scope) {
     $scope.quotes = []
     $scope.ratings	=[1, 2, 3, 4, 5]
     $scope.raven = false
-
+    $scope.rating =0
 
 
 
     $scope.isEnter = function($event){
     	console.log($event.which)
     	if ($scope.author && $scope.text && $event.which === 13) {
-    				$scope.quotes.push({author: $scope.author, text: $scope.text})
+    				$scope.quotes.push({author: $scope.author, text: $scope.text, rating: $scope.rating})
 					
     		
-    		if ($scope.author === "Edgar Allen Poe" || $scope.author ==="Poe" || $scope.author ==="Ravens" || $scope.author ==="edgar") {
+    		if ($scope.author === "Edgar Allan Poe" || $scope.author ==="Poe" || $scope.author ==="Ravens" || $scope.author ==="edgar") {
     			$scope.raven = false
     			$scope.text = ""
     			$scope.author = ""
