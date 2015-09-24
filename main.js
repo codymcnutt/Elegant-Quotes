f68556b14c4e7739c159898447070cf852b40a88
Student <student@Mac-mini.local>
2015-09-23 18:00:03 -0600

    add stuff

--- main.js
+++ main.js
@@ -5,26 +5,25 @@ angular.module('myApp').controller('newQuote', function ($scope) {
     $scope.raven = false
 
 
-   $scope.showRaven = function(){
-        $scope.Raven = true
-    }
-    $scope.dismissRaven = function(){
-        $scope.raven = false
-    }
-
 
 
     $scope.isEnter = function($event){
     	console.log($event.which)
     	if ($scope.author && $scope.text && $event.which === 13) {
-    		$scope.quotes.push({author: $scope.author, text: $scope.text})
-    		$scope.text = ""
-    		$scope.author = ""
-    		$scope.raven = !$scope.raven
-    	if ($scope.author === "Edgar Allen Poe" || $scope.author ==="Poe" || $scope.author ==="Ravens" || $scope.author ==="edgar") {
-return $scope.raven = true
-	}
-}
+    				$scope.quotes.push({author: $scope.author, text: $scope.text})
+					
+    		
+    		if ($scope.author === "Edgar Allen Poe" || $scope.author ==="Poe" || $scope.author ==="Ravens" || $scope.author ==="edgar") {
+    			$scope.raven = false
+    			$scope.text = ""
+    			$scope.author = ""
+    		} 
+    		else{
+    			$scope.raven = true
+    			$scope.text = ""
+    			$scope.author = ""
+    		}
+		} 
 
 }
     	console.log($scope.quotes)
